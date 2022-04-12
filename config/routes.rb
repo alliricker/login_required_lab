9e14a8d55057c4226fb852711626e96257c0da94
Allison Ricker <alliricker@gmail.com>
2022-04-12 13:04:23 -0600

    commit tree ff759dbed5af78fe0929d36a48b7494d2469a237

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'application#home'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/', to: 'secrets#show'
+  post '/logout', to: 'sessions#destroy'
+
 end
